3822fa67275e533d921ad1c325bf7c6e77301545
ShahlaNikbakht <nikbakht.shahla@gmail.com>
2024-04-05 06:25:06 +0330

    added update user button in html_putAPI

--- routes/user.js
+++ routes/user.js
@@ -1,101 +1,86 @@
-const express = require('express')
+const express = require("express");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
-
 let users = require("./../users");
 
-router.get('/',(req,res)=>{
-  res.render('users',{users:users})
-})
+router.get("/", (req, res) => {
+  res.render("users", { users: users });
+});
 
 router.get("/", (req, res) => {
-    res.status(200).json({ data: users, success: true });
-  });
-  
-  router.get("/:id", (req, res) => {
-    let user = users.find((user) => user.id === parseInt(req.params.id));
-    if (user) {
-      res.status(200).json({ data: user, message: "user found successfully" });
-    } else {
-      res.status(404).json({ message: "user not found" });
-    }
-  });
-  
-  router.post(
-    "/",
-    [
-      body("first_name").trim().not().isEmpty().withMessage("Must Not be empty"),
-      body("email").isEmail().withMessage("must be in email format"),
-      body("password") .isLength({ min: 8 }).withMessage("must be atleast 8 character"),
-    ],
-    (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.redirect('/user');
-        //status(400).json({ errors: errors.array() });
-      }
-      req.body.id = parseInt(req.body.id); // Ensure you're receiving 'id' in your request body or adjust accordingly.
-      users.push(req.body);
-      console.log("okkkkkkkkkkk")
-  
-      res.redirect('/user')
-     
-    }
-  );
-  
-  router.put(
-    "/:id",
-    [
-      body("email").isEmail().withMessage("email format is wrong"),
-      body("password")
-        .isLength({ min: 8 })
-        .withMessage("must be atleast 8 character"),
-      body("first_name").trim().not().isEmpty().withMessage("must not be empty"),
-    ],
-    (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
-      }
-      let found = false;
-      users = users.map((user) => {
-        if (user.id == parseInt(req.params.id)) {
-          found = true;
-          return { ...user, ...req.body };
-        }
-        return user;
-      });
-      if (!found) {
-        res.status(404).json({ message: "User not found" });
-      }
-      res.status(202).json({ message: "user got updated successfully" });
-    }
-  );
+  res.status(200).json({ data: users, success: true });
+});
 
-  router.delete("/:id", (req, res) => {
-    console.log("yessssssssssssss1111");
-    const initialLength = users.length;
-    console.log(req.params.id);
-    users = users.filter(user => user.id !== parseInt(req.params.id));
-    console.log("yessssssssssssss2222");
+router.get("/:id", (req, res) => {
+  let user = users.find((user) => user.id === parseInt(req.params.id));
+  if (user) {
+    console.log("renderrrrrrrrrrrrrrrrrr")
+    res.render('user',{user:user});
+  } else {
+    res.status(404).json({ message: "user not found" });
+  }
+});
 
-    if (users.length < initialLength) {
-      console.log("yessssssssssssss333");
-      // User found and deleted
-      res.redirect('/user')
-    } else { 
-      // User not found
+router.post(
+  "/",
+  [
+    body("first_name").trim().not().isEmpty().withMessage("Must Not be empty"),
+    body("email").isEmail().withMessage("must be in email format"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("must be atleast 8 character"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.redirect("/user");
+      //status(400).json({ errors: errors.array() });
+    }
+    req.body.id = parseInt(req.body.id); // Ensure you're receiving 'id' in your request body or adjust accordingly.
+    users.push(req.body);
+    res.redirect("/user");
+  }
+);
+
+router.put(
+  "/:id",
+  [
+    body("email").isEmail().withMessage("email format is wrong"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("must be atleast 8 character"),
+    body("first_name").trim().not().isEmpty().withMessage("must not be empty"),
+  ],
+  (req, res) => {
+    console.log("ddddddddddddddddddddddddddddddd")
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+    let found = false;
+    users = users.map((user) => {
+      if (user.id == parseInt(req.params.id)) {
+        found = true;
+        
+        return { ...user, ...req.body };
+      }
+      return user;
+    });
+    if (!found) {
       res.status(404).json({ message: "User not found" });
     }
-  });
-  
-  // router.delete("/:id", (req, res) => {
-  //   users = users.filter((user) => {
-  //     if (user.id !== parseInt(req.params.id)) {
-  //       return user;
-  //     }
-  //   });
-  //   return res.redirect('/user')
-  // });
+    console.log("putttttttttttttttttttttttttt")
+    res.redirect("/user");
+  }
+);
 
-  module.exports=router
+router.delete("/:id", (req, res) => {
+  users = users.filter((user) => {
+    if (user.id !== parseInt(req.params.id)) {
+      return user;
+    }
+  });
+  return res.redirect("/user");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const app = express();
-const methodOverride = require('method-override');
-app.use(methodOverride('_method'));
+const methodOverride = require("method-override");
 
 global.config = require("./config");
+app.use(methodOverride("_method"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
-app.use('/user', require('./routes/user'))
-app.set('view engine','ejs')
+app.use("/user", require("./routes/user"));
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
-  res.render('index',{user})
-})
+
 app.listen(config.port, () => {
   console.log(`server is running on port ${config.port}`);
 });
